b6cf41b4802b0d53c7d0b3880aa113fef745bca9
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-20 20:19:42 +0700

    Belajar Just In Time Complitation

---
+++ 9.JustInTimeComplitation/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// Todo Just In Time Complitaion
+// * PHP 8 mengenalkan fitur Just in Time Compilation
+// * Dimana fitur ini akan mempercepat proses eksekusi program PHP yang kita buat
+// * Namun sebelum kita bahas JIT, kita perlu tahu dulu bagaimana cara kerja PHP menjalankan kode program kita
+
+
+
+// Todo OPcache
+// * Secara default PHP akan selalu membaca kode PHP ketika menjalankan program PHP
+// * OPCache digunakan untuk meningkatkan performance PHP, dengan cara menyimpan hasil kompilasi kode PHP di memory. 
+// * Dengan demikian, PHP tidak perlu lagi membaca ulang kode program PHP setiap kali program dijalankan
+// * PHP akan langsung membaca dari OPcache yang sudah disimpan di memory
+// * Fitur OPcache harus diaktifkan terlebih dahulu, sebelum kita bisa menggunakannya
+// * https://www.php.net/manual/en/book.opcache.php 
+
+
+
+// Todo Just In Time Complitaion
+// * Opcache akan membuat kode program kita terhindar dari harus melakukan tokenize, parsing dan compile lagi secara terus menerus tiap request
+// * JIT, akan membuat hasil kompilasi kita tidak perlu diterjemahkan oleh virtual machine PHP, melainkan langsung dijalankan oleh machine
+// * JIT di PHP menggunakan library bahasa pemrograman C bernama DynASM, oleh karena itu JIT bisa mentranslate hasil compile opcodes ke instruksi machine
