8597db4f48d69fdb008fe50f097e0a82b4941e6d
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-20 21:43:00 +0700

    Belajar Comma Parameter List

---
+++ 12.CommaParameterList/Data.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// Todo Comma Di Parameter List
+// * Ini adalah salah satu fitur sederhana, tapi bermanfaat
+// * Di PHP 8, kita sekarang bisa menambahkan karakter koma di akhir parameter list, seperti ketika memanggil function, membuat array dan lain-lain
+// * https://wiki.php.net/rfc/trailing_comma_in_parameter_list 
+// * https://wiki.php.net/rfc/trailing_comma_in_closure_use_list 
+
+
+function sayHello(string $name , int $birthday)
+{
+
+}
+
+
+sayHello("Thomas" , 10);
